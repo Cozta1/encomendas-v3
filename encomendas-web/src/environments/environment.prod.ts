76d30a525fbf7c35cf14c615a4f100e46517324c
Joao Costa <zkozta@gmail.com>
2025-11-27 18:16:59 -0300

    Correção: Apontar url de producao pro backend Railway

--- encomendas-web/src/environments/environment.prod.ts
+++ encomendas-web/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // SUBSTITUA PELA URL REAL DO SEU BACKEND NO RAILWAY
-  // Exemplo: 'https://encomendas-api-production.up.railway.app/api'
-  apiUrl: 'encomendas-v3-production.up.railway.app'
+  apiUrl: 'https://encomendas-v3-production.up.railway.app/api'
 };
