bec9190cdf0c238d1df7694415e28c8b73905376
Joao Costa <zkozta@gmail.com>
2025-12-02 12:53:33 -0300

    Correção: tentativa de correção de erros de build

---
+++ encomendas-web/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  // Pode deixar a URL do Railway aqui também para facilitar,
+  // ou usar localhost se for rodar na sua máquina.
+  apiUrl: 'https://encomendas-api-production.up.railway.app/api'
+};
