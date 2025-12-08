4dcfbbf302f06d28d5f7fb63f0aa307fb34a3549
Joao Costa <zkozta@gmail.com>
2025-12-08 10:46:04 -0300

    back e front end funcionam, mas não se comunicam

--- encomendas-web/src/environments/environment.prod.ts
+++ encomendas-web/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // Remova o 'https://' se sua lógica de serviço concatenar,
-  // MAS seu AuthService usa `${environment.apiUrl}/auth`, então coloque a URL base SEM a barra no final.
-  apiUrl: 'https://encomendas-api-969073157004.us-central1.run.app '
+  // A URL deve terminar com /api pois seus serviços (AuthService, etc) esperam isso.
+  apiUrl: 'https://encomendas-api-969073157004.us-central1.run.app/api'
 };
